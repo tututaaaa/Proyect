d676787ddf4270145dca25e8ed6fbb6b1914f009
santi <tizianoperea@hotmail.com>
2023-10-25 20:14:29 -0300

    v0.1

---
+++ E-commerce/src/components/Card.jsx
@@ -0,0 +1,12 @@
+
+export const Card = () => {
+  return (
+    <article className="article">
+        <img src="" alt="" className="article-img" />
+        <h2 className="article-titulo"></h2>
+        <p className="article-price"></p>
+        <p className="article-description"></p>
+        <div className="article-rating"></div>
+    </article>
+  )
+}
