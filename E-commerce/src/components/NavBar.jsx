a71ef8e5ba69fd8619b6c3203c3e24864aca5aa2
nahuuuu <nahuellmallea@gmail.com>
2023-10-25 20:38:45 -0300

    v 0.1

--- E-commerce/src/components/NavBar.jsx
+++ E-commerce/src/components/NavBar.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+
+import { StyledBadge } from '@mui/material/styles';
+import IconButton from '@mui/material/IconButton';
+import ShoppingCartIcon from "@mui/material/ShoppingCartIcon"
 
 export const NavBar = () => {
-  return (
-    <div>NavBar</div>
-  )
+    return (
+        <>
+            <div>
+                <a href="/">Inicio</a>
+                <IconButton aria-label="cart">
+                    <StyledBadge badgeContent={4} color="secondary">
+                        <ShoppingCartIcon />
+                    </StyledBadge>
+                </IconButton>
+            </div>
+
+
+
+        </>
+    )
 }
--- E-commerce/src/main.jsx
+++ E-commerce/src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './eCommerceApp.jsx'
-import './index.css'
+import { Ecommerce } from './Ecommerce'
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>  
-    <App />
+  <React.StrictMode>
+    < Ecommerce />
   </React.StrictMode>,
 )
